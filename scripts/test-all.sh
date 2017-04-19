76e75203efb85fe1e14c5149a2ad1d03f524640d
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-04-19 12:16:40 +0200

    Simplify.

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -7,5 +7,5 @@ IFS=$'\n\t'
 ####### run tests ################
 
 set -x
-bioconda-utils build recipes config.yml --testonly --force 2>&1 | tee test.log
+bioconda-utils build recipes config.yml --testonly --force
 set +x
