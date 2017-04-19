ee935803fb34012804ecfb559b516ea97fab515b
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-04-19 08:42:24 +0200

    Try to remove all docker data.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -10,6 +10,7 @@ conda clean --all --yes
 ####### clean docker cache ######
 # delete stopped containers
 docker container prune -f
+docker system prune -f
 # delete images older than a week
 IMAGES=$(docker images | (grep " [months|weeks|years]* ago" || true) | awk '{print $3}')
 if [[ "$IMAGES" != "" ]]
