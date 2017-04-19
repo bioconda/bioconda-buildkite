e7a35b8d904de3f630c723afbedd94b4a8e6276f
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-04-19 08:39:50 +0200

    Minor.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -31,11 +31,11 @@ done
 MINICONDA_PATH=/tmp/.miniconda
 
 OLD_MINICONDA_VER=$(cat $MINICONDA_PATH/version.txt 2> /dev/null || echo "")
-echo "previous Miniconda version: $OLD_MINICONDA_VER"
+echo "Miniconda version: $OLD_MINICONDA_VER"
 
 if [[ "$OLD_MINICONDA_VER" != "$MINICONDA_VER" ]]
 then
-    echo "installing Miniconda version: $MINICONDA_VER"
+    echo "updating Miniconda version: $MINICONDA_VER"
     curl -O https://repo.continuum.io/miniconda/Miniconda3-$MINICONDA_VER-$OS-x86_64.sh
     bash Miniconda3-$MINICONDA_VER-$OS-x86_64.sh -b -p $MINICONDA_PATH
     echo $MINICONDA_VER > $MINICONDA_PATH/version.txt
