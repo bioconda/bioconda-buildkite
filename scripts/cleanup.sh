652cf7ffb318b79e971cd36fb48ff62dbe2f2341
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-04-19 08:49:10 +0200

    Simplify cleanup.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -8,12 +8,4 @@ IFS=$'\n\t'
 conda clean --all --yes
 
 ####### clean docker cache ######
-# delete stopped containers
-docker container prune -f
 docker system prune -f
-# delete images older than a week
-IMAGES=$(docker images | (grep " [months|weeks|years]* ago" || true) | awk '{print $3}')
-if [[ "$IMAGES" != "" ]]
-then
-    docker rmi $IMAGES
-fi
